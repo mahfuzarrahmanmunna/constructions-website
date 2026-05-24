8ba19cefe342fcb60d00e8cecb17802522349206
mahfuzarrahmanmunna <mdmahfuzarrahmanmunna44@gmail.com>
2026-05-25 02:56:35 +0600

    added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import HeroBanner from '@/app/components/HeroBanner/HeroBanner'
+import ExploreProducts from './components/ExploreProducts/ExploreProducts'
 
 export default function Home() {
   return (
     <main className="flex flex-col flex-1">
       <HeroBanner />
+      <ExploreProducts/>
     </main>
   )
 }
