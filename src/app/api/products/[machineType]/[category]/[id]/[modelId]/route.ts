7ca397832871267fb331669be41fe632a0ff29d0
KM-Adnan-Absar-Jilan <jilan@it.puc.ac.bd>
2026-06-11 11:34:12 +0600

    added

---
+++ src/app/api/products/[machineType]/[category]/[id]/[modelId]/route.ts
@@ -0,0 +1,40 @@
+import { NextResponse } from "next/server";
+import { dbConnect } from "@/lib/dbConnect";
+import Product from "@/models/Product";
+
+type Params = {
+  machineType: string;
+  category: string;
+  id: string;
+  modelId: string;
+};
+
+export async function GET(
+  _req: Request,
+  { params }: { params: Params }
+) {
+  try {
+    await dbConnect();
+
+    const { machineType, category, id, modelId } = await params;
+
+    const product = await Product.findOne({
+  productId: Number(id),
+  modelId: Number(modelId),
+  machineType,
+  category,
+});
+
+    if (!product) {
+      return NextResponse.json(
+        { message: "Product not found" },
+        { status: 404 }
+      );
+    }
+
+    return NextResponse.json(product);
+  } catch (error: unknown) {
+    const message = error instanceof Error ? error.message : "Server Error";
+    return NextResponse.json({ message }, { status: 500 });
+  }
+}
